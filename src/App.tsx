679b4dab3da502376d7aeeeba46c711ee3f931a4
winchesterrx <123671820+winchesterrx@users.noreply.github.com>
2025-05-26 22:55:46 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import BarberServices from "./pages/admin/BarberServices";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Index from "./pages/Index";
@@ -30,6 +31,7 @@ const App = () => (
           <Route path="/admin/dashboard" element={<AdminDashboard />} />
           <Route path="/admin/horarios" element={<AdminSchedule />} />
           <Route path="/admin/perfil" element={<AdminProfile />} />
+          <Route path="/admin/servicos" element={<BarberServices />} /> {/* ✅ nova rota */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
